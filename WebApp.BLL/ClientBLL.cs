dfbbd42b171b2773ec6ca8e7044ddfd7623556be
asus <asus@GUERRIC-PC>
2017-03-14 22:25:41 +0800

    Added validations and columns in client list table

--- WebApp.BLL/ClientBLL.cs
+++ WebApp.BLL/ClientBLL.cs
@@ -19,24 +19,39 @@ namespace WebApp.BLL
         {
             _repository = new GenericRepository<ClientEntity>();
         }
-
+        /// <summary>
+        /// Adding new Client into Database
+        /// </summary>
+        /// <param name="client"></param>
         public void AddClient(ClientModel client)
         {
-            if (ValidateIfExists(client))
+            var exists = ValidateIfExists(client);
+            var complete = ValidateCompleteFields(client);
+            if (exists && complete)
             {
                 TinyMapper.Bind<ClientModel, ClientEntity>();
                 var clientEntity = TinyMapper.Map<ClientEntity>(client);
                 clientEntity.Created_Date = DateTime.Now;
                 _repository.Insert(clientEntity);
             }
+            else if (complete == false)
+            {
+                throw new Exception("Please fill out required fields.");
+            }
             else
             {
                 throw new Exception("Client already exists.");
             }
         }
+        /// <summary>
+        /// Updating Client Info
+        /// </summary>
+        /// <param name="client"></param>
         public void UpdateClient(ClientModel client)
         {
-            if (ValidateIfExists(client))
+            var exists = ValidateIfExists(client);
+            var complete = ValidateCompleteFields(client);
+            if (exists && complete)
             {
                 Mapper.Initialize(cfg => cfg.CreateMap<ClientModel, ClientEntity>().ForMember(x=> x.Created_Date, opt => opt.Ignore()));
                 var clientEntity = _repository.Get(i => i.Id == client.Id).First();
@@ -44,11 +59,42 @@ namespace WebApp.BLL
                 clientEntity.Modified_Date = DateTime.Now;
                 _repository.Update(clientEntity);
             }
+            else if (complete == false)
+            {
+                throw new Exception("Please fill out required fields.");
+            }
             else
             {
                 throw new Exception("Client already exists.");
             }
         }
+        /// <summary>
+        /// Add Validation Function for incomplete fields
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public bool ValidateCompleteFields(ClientModel client)
+        {
+            var complete = true;
+            if (client.Name == "" || client.Name == null)
+            {
+                complete = false;
+            }
+            if (client.Telephone_Number == "" || client.Telephone_Number == null)
+            {
+                complete = false;
+            }
+            if (client.Contacts_Order == "" || client.Contacts_Order == null)
+            {
+                complete = false;
+            }
+            return complete;
+        }
+        /// <summary>
+        /// Add Validation function for checking if client exists already
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
         public bool ValidateIfExists(ClientModel client)
         {
             var clients = _repository.Get();
