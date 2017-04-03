c74c1e40f53ae22628df0f0eff367de89631caa7
asus <asus@GUERRIC-PC>
2017-04-03 22:19:46 +0800

    Added Migration for Clients and Products and improve performance in retrieving clients and products

--- MigrationTool/Migration.Designer.cs
+++ MigrationTool/Migration.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MigrationTool
 {
-    partial class Form1
+    partial class Migration
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.migrate = new System.Windows.Forms.Button();
+            this.settings = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // migrate
             // 
-            this.migrate.Location = new System.Drawing.Point(93, 28);
+            this.migrate.Location = new System.Drawing.Point(135, 12);
             this.migrate.Name = "migrate";
             this.migrate.Size = new System.Drawing.Size(117, 39);
             this.migrate.TabIndex = 0;
@@ -41,13 +42,24 @@
             this.migrate.UseVisualStyleBackColor = true;
             this.migrate.Click += new System.EventHandler(this.migrate_Click);
             // 
-            // Form1
+            // settings
+            // 
+            this.settings.Location = new System.Drawing.Point(12, 12);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(117, 39);
+            this.settings.TabIndex = 1;
+            this.settings.Text = "DB Settings";
+            this.settings.UseVisualStyleBackColor = true;
+            this.settings.Click += new System.EventHandler(this.settings_Click);
+            // 
+            // Migration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 108);
+            this.ClientSize = new System.Drawing.Size(542, 226);
+            this.Controls.Add(this.settings);
             this.Controls.Add(this.migrate);
-            this.Name = "Form1";
+            this.Name = "Migration";
             this.Text = "Migration Tool";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -57,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button migrate;
+        private System.Windows.Forms.Button settings;
     }
 }
 
---
+++ MigrationTool/MySQL/ConnectionSetup.cs
@@ -0,0 +1,101 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationTool.MySQL
+{
+    class ConnectionSetup : IDisposable
+    {
+        private MySqlConnection connection;
+        private MySqlCommand query;
+        private string dbServer;
+        private string dbUsername;
+        private string dbPassword;
+        private string dbName;
+        void IDisposable.Dispose()
+        {
+        }
+        public ConnectionSetup(string server, string username, string password, string database)
+        {
+            this.dbServer = server;
+            this.dbUsername = username;
+            this.dbPassword = password;
+            this.dbName = database;
+        }
+        public List<object[]> executeReader()
+        {
+            MySqlDataReader reader = query.ExecuteReader();
+
+            List<object[]> result = new List<object[]>();
+            while (reader.Read())
+            {
+                object[] dataRow = new object[reader.FieldCount];
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    dataRow[i] = reader.GetValue(i);
+                }
+                result.Add(dataRow);
+            }
+
+            reader.Close();
+
+            return result;
+        }
+
+        public void open()
+        {
+            connection = new MySqlConnection(
+                "Server=" + dbServer + ";" +
+                //"PORT = 3306 ;" +
+                "Database=" + dbName + ";" +
+                "Uid=" + dbUsername + ";" +
+                "Pwd=" + dbPassword + ";" +
+                "Convert Zero Datetime=True;" +
+                "Allow Zero Datetime=True;");
+
+            connection.Open();
+        }
+        public object[] executeScalar()
+        {
+            MySqlDataReader reader = query.ExecuteReader();
+
+            object[] newObject = new object[1];
+            while (reader.Read())
+            {
+                object[] dataRow = new object[reader.FieldCount];
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    dataRow[i] = reader.GetValue(i);
+                }
+                newObject = dataRow;
+            }
+
+            reader.Close();
+
+            return newObject;
+        }
+        public void close()
+        {
+            connection.Close();
+        }
+        public void setQuery(string sqlQuery)
+        {
+            query = new MySqlCommand(sqlQuery, connection);
+        }
+
+        public void setParameter(string parameterName, object parameterValue)
+        {
+            query.Parameters.Add(new MySqlParameter("?" + parameterName, parameterValue));
+        }
+        public void executeNonQuery()
+        {
+            query.ExecuteNonQuery();
+        }
+    }
+}
